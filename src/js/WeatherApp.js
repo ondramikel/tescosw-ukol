159c74e11a21a538cc9176d16aeaf9494809a918
Ondřej Mikel <ondra@mikelovi.cz>
2025-12-09 16:21:22 +0100

    přidání možnosti geolokace

--- src/js/WeatherApp.js
+++ src/js/WeatherApp.js
@@ -13,6 +13,8 @@ export default class WeatherApp {
         this.suggestions;
         this.forecast;
         this.chose;
+        this.geo;
+
 
     
     }
@@ -24,15 +26,20 @@ export default class WeatherApp {
         this.suggestions = document.getElementById("suggestions");
         this.forecast = document.getElementById("forecast");
         this.chose = document.getElementById("chose");
+        this.geo = document.getElementById("geolocate");
+
 
         this.ui = new Ui(this.input, this.suggestions, this.forecast);
 
 
-        
+
         if(!this.input) return false;
     
         this.eventOnInput();
         
+        if(!this.geo) return false;
+
+        this.byGeolocate()
 
         if(!this.chose) return false;
 
@@ -49,6 +56,39 @@ export default class WeatherApp {
         })
     }
 
+
+    byGeolocate() {
+        this.geo.addEventListener("click",(e) => {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    
+
+                    let geoData = {
+                        "coord": {
+                            "lon": position.coords.longitude,
+                            "lat": position.coords.latitude
+                        }
+                    }
+
+                    console.log("Lat:", geoData.coord.lat);
+                    console.log("Lon:", geoData.coord.lon);
+
+
+                    this.weatherData.getForecast(geoData).then(data => {
+                        console.log(data);
+                        console.log(data.city.name);
+                        this.ui.setCityName(data.city.name);
+
+                        this.ui.renderWeather(data.list);
+                    });
+                },
+                (error) => {
+                    console.error("Chyba geolokace:", error);
+                }
+            );
+        })
+    }
+
     eventOnChose() {
         this.chose.addEventListener("click",(e) => {
             let input_data = this.ui.getInput();
@@ -57,6 +97,7 @@ export default class WeatherApp {
             if (find_data) {
                 this.weatherData.getForecast(find_data).then(data => {
                     console.log(data);
+                    this.ui.setCityName(input_data);
                     this.ui.renderWeather(data.list);
                 });
             }
