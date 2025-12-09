71503efdce31c0ba3836cc62afe354b711b18b79
Ondřej Mikel <ondra@mikelovi.cz>
2025-12-09 11:10:09 +0100

    vytvoření třídy pro získání dat přes api

---
+++ src/js/WeatherData.js
@@ -0,0 +1,23 @@
+export default class WeatherData {
+    constructor() {
+        this.apiKey = "14095ed384f72adb429b55fdd17c57bf";  
+        this.baseUrl = "https://api.openweathermap.org/data/2.5/forecast";
+    }
+
+    async getForecast(city) {
+        const lat = city.lat;
+        const lon = city.lon;
+        const units = "metric";
+        const lang = "cs";
+
+        const url = `${this.baseUrl}?lat=${lat}&lon=${lon}&units=${units}&lang=${lang}&appid=${this.apiKey}`;
+
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Chyba při získávání dat");
+        }
+
+        const data = await response.json();
+        return data;
+    }
+}
